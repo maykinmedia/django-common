4477983edde9c09ede9ade187eb4939ac19c788e
Sergei Maertens <sergei@maykinmedia.nl>
2025-05-28 14:24:03 +0200

    :fire: Remove the model form meta.exclude check
    This check performed the same check as Ruff's DJ006. Now that we're
    moving everything to Ruff, it's recommended to use that linter rule
    rather than relying on a system check that slows down runserver
    reloads and application startup time. Additionally, it removes some
    friction with third party form definitions that violate this
    requirement that we can't fix/address.

--- maykin_common/checks.py
+++ maykin_common/checks.py
@@ -1,48 +1,7 @@
 import os
 
 from django.conf import settings
-from django.core.checks import Error, Warning, register
-from django.forms import ModelForm
-
-
-def get_subclasses(cls):
-    for subclass in cls.__subclasses__():
-        yield from get_subclasses(subclass)
-        yield subclass
-
-
-@register()
-def check_modelform_exclude(app_configs, **kwargs):
-    """
-    Check that ModelForms use Meta.fields instead of Meta.exclude.
-
-    ModelForm.Meta.exclude is dangerous because it doesn't protect against
-    fields that are added later. Explicit white-listing is safer and prevents
-    bugs such as IMA #645.
-
-    This check piggy-backs on all form modules to be imported during Django
-    startup. It won't cover forms that are defined on the fly such as in
-    formset factories.
-    """
-    errors = []
-
-    for form in get_subclasses(ModelForm):
-        # ok, fields is defined
-        if form._meta.fields or getattr(form.Meta, "fields", None):
-            continue
-
-        # no `.fields` defined, so scream loud enough to prevent this
-        errors.append(
-            Error(
-                f"ModelForm {form.__module__}.{form.__name__} with Meta.exclude "
-                "detected, this is a bad practice",
-                hint="Use ModelForm.Meta.fields instead",
-                obj=form,
-                id="maykin.E001",
-            )
-        )
-
-    return errors
+from django.core.checks import Warning, register
 
 
 @register
