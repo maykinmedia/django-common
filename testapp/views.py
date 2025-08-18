3148ef3b478e65b0917202a2a6dcd4860ae04583
Daniel Mursa <daniel@maykinmedia.nl>
2025-08-18 15:50:44 +0200

    :hammer: Add API component template and view

--- testapp/views.py
+++ testapp/views.py
@@ -1,5 +1,11 @@
 from django.views.generic import TemplateView
 
+from maykin_common.api_reference.views import ComponentIndexView
+
 
 class ApiLandingPageView(TemplateView):
     template_name = "testapp/api/index.html"
+
+
+class CustomComponentIndexView(ComponentIndexView):
+    template_name = "testapp/api/component.html"
