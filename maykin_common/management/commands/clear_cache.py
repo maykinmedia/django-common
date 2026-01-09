71687a487af6911fcb5002a802fc89dbb81909c6
Sergei Maertens <sergei@maykinmedia.nl>
2026-01-09 11:21:49 +0100

    :package: [#42] Ensure the compiled translations files are distributed

--- maykin_common/management/commands/clear_cache.py
+++ maykin_common/management/commands/clear_cache.py
@@ -1,13 +1,12 @@
 from django.core.cache import DEFAULT_CACHE_ALIAS, caches
 from django.core.management import BaseCommand
-from django.utils.translation import gettext as _
 
 
 class Command(BaseCommand):
     help = "Clears the Django cache"
 
     def add_arguments(self, parser):
-        parser.add_argument("--alias", help=_("Clear given cache only"))
+        parser.add_argument("--alias", help="Clear given cache only")
 
     def handle(self, *args, **options):
         alias = options["alias"]
