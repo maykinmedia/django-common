b9387945e05d674145be981700a50194a81f1af0
Sergei Maertens <sergei@maykinmedia.nl>
2026-06-25 10:37:17 +0200

    :white_check_mark: [#77] Add tests for branding template tag
    Cover the various combinations of with/without logo/hyperlink and
    ensure that the product names are not an XSS attack vector.

---
+++ tests/base/test_branding_templatetag.py
@@ -0,0 +1,182 @@
+from collections.abc import Mapping
+
+from django.template import Context, Template
+
+import pytest
+from pytest_django.asserts import assertInHTML
+
+from maykin_common.branding import ProductDefinition
+
+
+def _render_product_branding(context=None):
+    tpl = Template(
+        """
+        {% load maykin_common %}
+        {% show_product_branding %}
+    """
+    )
+    return tpl.render(Context(context or {})).strip()
+
+
+@pytest.mark.parametrize(
+    "settings_values",
+    (
+        {
+            "MKN_BRANDING_PRODUCT_DEFINITION": None,
+            "MKN_BRANDING_DERIVED_PRODUCT_DEFINITION": None,
+        },
+        {
+            "MKN_BRANDING_PRODUCT_DEFINITION": None,
+            "MKN_BRANDING_DERIVED_PRODUCT_DEFINITION": ProductDefinition(
+                name="Ignore me"
+            ),
+        },
+    ),
+)
+def test_no_product_definition_defined(settings, settings_values: Mapping[str, object]):
+    """
+    Assert the tag renders nothing if no white label product definition is defined.
+    """
+    for setting, value in settings_values.items():
+        setattr(settings, setting, value)
+
+    result = _render_product_branding()
+
+    assert result == ""
+
+
+def test_white_label_product_defined_but_no_derived_product(settings):
+    settings.MKN_BRANDING_PRODUCT_DEFINITION = ProductDefinition(name="Product name")
+    settings.MKN_BRANDING_DERIVED_PRODUCT_DEFINITION = None
+
+    result = _render_product_branding()
+
+    assert "Product name" in result
+    assertInHTML(
+        """
+        <img
+            src="/static/maykin_common/logo-wide.svg"
+            alt="Maykin"
+            class="product-branding__logo product-branding__logo--maykin"
+        >
+        """,
+        result,
+    )
+
+
+def test_logo_rendered_and_product_name_clickable_link(settings):
+    settings.MKN_BRANDING_PRODUCT_DEFINITION = ProductDefinition(
+        name="Product name",
+        hyperlink="https://example.com",
+        logo_path="maykin_common/ico/favicon.png",
+    )
+    settings.MKN_BRANDING_DERIVED_PRODUCT_DEFINITION = None
+
+    result = _render_product_branding()
+
+    assertInHTML(
+        """
+        <img
+            src="/static/maykin_common/ico/favicon.png"
+            alt="Favicon of the product Product name"
+            class="product-branding__logo product-branding__logo--favicon"
+        >
+        """,
+        result,
+    )
+    assertInHTML(
+        """
+        <a href="https://example.com" target="_blank" rel="noopener nofollower">
+            Product name
+        </a>
+        """,
+        result,
+    )
+
+
+def test_minimal_product_definition_with_custom_product(settings):
+    settings.MKN_BRANDING_PRODUCT_DEFINITION = ProductDefinition(name="Product name")
+    settings.MKN_BRANDING_DERIVED_PRODUCT_DEFINITION = ProductDefinition(
+        name="Custom name"
+    )
+
+    result = _render_product_branding()
+
+    assert "Product name" in result
+    assert "Custom name" in result
+
+
+def test_escapes_content(settings):
+    settings.MKN_BRANDING_PRODUCT_DEFINITION = ProductDefinition(
+        name="Product <xss> name"
+    )
+    settings.MKN_BRANDING_DERIVED_PRODUCT_DEFINITION = ProductDefinition(
+        name="Custom <xss> name"
+    )
+
+    result = _render_product_branding()
+
+    assert "&lt;xss&gt;" in result
+    assert "<xss>" not in result
+
+
+def test_renders_both_logos_and_links(settings):
+    settings.MKN_BRANDING_PRODUCT_DEFINITION = ProductDefinition(
+        name="Product name",
+        hyperlink="https://example.com/white-label",
+        logo_path="maykin_common/ico/favicon.png",
+    )
+    settings.MKN_BRANDING_DERIVED_PRODUCT_DEFINITION = ProductDefinition(
+        name="Custom name",
+        hyperlink="https://example.com/custom",
+        logo_path="maykin_common/ico/favicon.svg",
+    )
+
+    result = _render_product_branding()
+
+    # white label product
+    assertInHTML(
+        """
+        <img
+            src="/static/maykin_common/ico/favicon.png"
+            alt="Favicon of the product Product name"
+            class="product-branding__logo product-branding__logo--favicon"
+        >
+        """,
+        result,
+    )
+    assertInHTML(
+        """
+        <a
+            href="https://example.com/white-label"
+            target="_blank"
+            rel="noopener nofollower"
+        >
+            Product name
+        </a>
+        """,
+        result,
+    )
+    # custom product
+    assertInHTML(
+        """
+        <img
+            src="/static/maykin_common/ico/favicon.svg"
+            alt="Favicon of the product Custom name"
+            class="product-branding__logo product-branding__logo--favicon"
+        >
+        """,
+        result,
+    )
+    assertInHTML(
+        """
+        <a
+            href="https://example.com/custom"
+            target="_blank"
+            rel="noopener nofollower"
+        >
+            Custom name
+        </a>
+        """,
+        result,
+    )
