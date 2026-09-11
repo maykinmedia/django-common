c97edec98365540a5368444518ac1001e0299f26
Sergei Maertens <sergei@maykinmedia.nl>
2026-09-11 14:41:24 +0200

    :sparkles: [#33] Include a default celery structlog setup

---
+++ maykin_common/logging/celery.py
@@ -0,0 +1,33 @@
+"""
+Structured logging helpers for Celery.
+"""
+
+import logging
+import logging.config
+
+from django.conf import settings
+
+from celery.signals import setup_logging
+
+from .config import structlog_configure_defaults
+
+
+def setup_celery_structlog(format_exc_info: bool = True):
+    """
+    Apply the ``settings.LOGGING`` config to Celery and initialize structlog.
+    """
+
+    def _receiver_setup_logging(
+        loglevel, logfile, format, colorize, **kwargs
+    ):  # pragma: no cover
+        # applies the settings.LOGGING configuration to Celery as well
+        logging.config.dictConfig(settings.LOGGING)
+        structlog_configure_defaults(format_exc_info=format_exc_info)
+
+    setup_logging.connect(
+        _receiver_setup_logging,
+        dispatch_uid="maykin_common.logging.celery_structlog",
+        weak=False,  # prevent _receiver_setup_logging from being garbage collected
+    )
+
+    return _receiver_setup_logging
